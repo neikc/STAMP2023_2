c5754be5e77bcdc6079f0a9f5c164e3ef00c58dc
Châu Chí Kiện <kienchau.31221022673@st.ueh.edu.vn>
2023-07-23 14:44:47 +0700

    Thêm các Page About Ranking Voting Home News trống chưa có gì; bên cạnh đó làm Header đơn giản

---
+++ STAMP2023/STAMP2023/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace STAMP2023.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public AboutModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}

5750d6e83457abfe0dcc701bd326e8120f7825e8
Châu Chí Kiện <kienchau.31221022673@st.ueh.edu.vn>
2023-08-13 20:50:45 +0700

    13.8.2023 Banner động

---
+++ STAMP2023/STAMP2023/Pages/Info.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace STAMP2023.Pages
+{
+    public class InfoModel : PageModel
+    {
+        private readonly ILogger<InfoModel> _logger;
+
+        public InfoModel(ILogger<InfoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ STAMP2023/STAMP2023/Pages/Login.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace STAMP2023.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public LoginModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
